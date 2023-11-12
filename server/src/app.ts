640d23bc0e9ac681047e0d8b282a4cfef330cc07
irving11119 <irving.deboer@gmail.com>
2023-11-12 21:21:13 +0800

    Integrate RabbitMQ Consumer for room service events

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,13 +2,16 @@
  * @file Manages the configuration settings for Socket IO API.
  * @author Irving de Boer
  */
-import express, { Application } from 'express';
-import { createServer } from 'http';
-import { Server } from 'socket.io';
-import { Room } from './controller/chat.room';
-import { createClient } from 'redis';
 import { createAdapter } from '@socket.io/redis-streams-adapter';
 import * as dotenv from 'dotenv';
+import express, { Application } from 'express';
+import { createServer } from 'http';
+import { createClient } from 'redis';
+import { Server } from 'socket.io';
+
+import { Room } from './controller/chat.room';
+import { RoomEvent } from './interfaces/chat.interfaces';
+import RoomServiceMqConsumer from './mq/chat.mq';
 
 dotenv.config();
 
@@ -16,7 +19,9 @@ class App {
   public app: Application;
   public server;
   public io;
-  private _redisClient = createClient({ url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_DOCKER_PORT}` });
+  private _redisClient = createClient({
+    url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_DOCKER_PORT}`,
+  });
 
   public constructor() {
     this.app = express();
@@ -26,21 +31,44 @@ class App {
       adapter: createAdapter(this._redisClient),
       cors: {
         origin: '*',
-      }
-    })
+      },
+      path: '/chat-service',
+    });
     this._setController();
   }
 
-  private _setController() {
+  private async _setController() {
     this.app.get('/', (req, res) => {
       res.send('PeerPrep Chat Service');
     });
 
     new Room(this.io);
+
+    await this._consumeMessages();
   }
 
   private async _setRedisConnect() {
     await this._redisClient.connect();
+  }
+
+  private async _consumeMessages() {
+    const consumer: RoomServiceMqConsumer = new RoomServiceMqConsumer({
+      exchangeName: process.env.MQ_EXCHANGE_NAME!,
+      host: process.env.MQ_HOST!,
+      password: process.env.MQ_PASSWORD!,
+      port: Number(process.env.MQ_PORT!),
+      queueName: 'chat-service-room-event-queue',
+      shouldUseTls: false,
+      user: process.env.MQ_USER!,
+      vhost: process.env.MQ_VHOST!,
+    });
+
+    await consumer.initialise();
+    console.log('Initialised');
+
+    await consumer.consume(async (data: RoomEvent) => {
+      console.log(`Received: ${JSON.stringify(data)}`);
+    });
   }
 }
 
--- server/src/interfaces/chat.interfaces.ts
+++ server/src/interfaces/chat.interfaces.ts
@@ -12,3 +12,36 @@ export interface MessagePayload {
   message: string;
   userName: string;
 }
+
+export interface RoomEvent {
+  readonly eventType: EventType;
+  readonly room: Room;
+  readonly removedUserId: number; // Only used by remove user event
+}
+
+/** Room. */
+export interface Room {
+  readonly roomId: string;
+  readonly userIds: number[];
+  readonly questionId: string;
+  readonly questionLangSlug: string;
+}
+
+/** Event type. */
+export enum EventType {
+  create = 'create',
+  delete = 'delete',
+  removeUser = 'remove-user',
+}
+
+/** Config for the Room Service MQ consumer. */
+export interface RoomServiceMqConsumerConfig {
+  readonly password: string;
+  readonly user: string;
+  readonly host: string;
+  readonly port: number;
+  readonly vhost: string;
+  readonly shouldUseTls: boolean;
+  readonly exchangeName: string;
+  readonly queueName: string;
+}
